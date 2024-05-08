6e787639e6c626bffd9e30a3d9959897d81f1ec0
Ed <edwardxperia2000@gmail.com>
2024-05-08 19:55:04 +0300

    fixes

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -154,7 +154,7 @@ namespace Content.Shared.CCVar
         ///     Controls the default game preset.
         /// </summary>
         public static readonly CVarDef<string>
-            GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "CP14Playtset", CVar.ARCHIVE); //Crystall Punk Playtest
+            GameLobbyDefaultPreset = CVarDef.Create("game.defaultpreset", "CP14Playtest", CVar.ARCHIVE); //Crystall Punk Playtest
 
         /// <summary>
         ///     Controls if the game can force a different preset if the current preset's criteria are not met.
