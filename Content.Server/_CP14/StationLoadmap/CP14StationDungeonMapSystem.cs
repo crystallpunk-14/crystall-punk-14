831854a4d2e240eee301e1a1da6c15b01e51bed5
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-05-14 17:13:46 +0300

    add maps linking
    shitcode, boo

--- Content.Server/_CP14/StationLoadmap/CP14StationDungeonMapSystem.cs
+++ Content.Server/_CP14/StationLoadmap/CP14StationDungeonMapSystem.cs
@@ -2,7 +2,10 @@ using Content.Server._CP14.StationBiome;
 using Content.Server.Parallax;
 using Content.Server.Station.Components;
 using Content.Server.Station.Events;
+using Content.Server.Station.Systems;
+using Content.Shared.Teleportation.Systems;
 using Robust.Server.GameObjects;
+using Robust.Shared.Map;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._CP14.StationLoadmap;
@@ -12,6 +15,8 @@ public sealed partial class CP14StationDungeonMapSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly MapSystem _map = default!;
     [Dependency] private readonly MetaDataSystem _metaData = default!;
+    [Dependency] private readonly LinkedEntitySystem _linkedEntity = default!;
+    [Dependency] private readonly StationSystem _station = default!;
 
     public override void Initialize()
     {
@@ -21,7 +26,7 @@ public sealed partial class CP14StationDungeonMapSystem : EntitySystem
 
     private void OnStationPostInit(Entity<CP14StationDungeonMapComponent> map, ref StationPostInitEvent args)
     {
-        if (!HasComp<StationDataComponent>(map))
+        if (!TryComp(map, out StationDataComponent? dataComp))
             return;
 
         var mapUid = _map.CreateMap(out var mapId, false);
@@ -30,5 +35,21 @@ public sealed partial class CP14StationDungeonMapSystem : EntitySystem
         _biome.EnsurePlanet(mapUid, _proto.Index(map.Comp.Biome), map.Comp.Seed, mapLight: map.Comp.MapLightColor);
 
         EntityManager.AddComponents(mapUid, map.Comp.Components);
+
+        TryLinkDungeonAndStationMaps(dataComp, mapUid);
+    }
+
+    private bool TryLinkDungeonAndStationMaps(StationDataComponent dataComp, EntityUid mapUid)
+    {
+        var station = _station.GetLargestGrid(dataComp);
+        if (station == null)
+            return false;
+
+        var stationMapId = Transform(station.Value).MapID;
+        _map.TryGetMap(stationMapId, out var stationMapUid);
+        if (stationMapUid == null)
+            return false;
+
+        return _linkedEntity.TryLink(mapUid, stationMapUid.Value);
     }
 }
