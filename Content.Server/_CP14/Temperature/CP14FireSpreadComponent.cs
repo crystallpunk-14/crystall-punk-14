e8f708c548488724ff02fe871cebaa7abf152052
Ed <edwardxperia2000@gmail.com>
2024-06-09 00:48:12 +0300

    FIRE!!! more structures can burn

--- Content.Server/_CP14/Temperature/CP14FireSpreadComponent.cs
+++ Content.Server/_CP14/Temperature/CP14FireSpreadComponent.cs
@@ -25,7 +25,6 @@ public sealed partial class CP14FireSpreadComponent : Component
     [DataField]
     public float ProbTile = 0.15f;
 
-
     /// <summary>
     /// how often objects will try to set the neighbors on fire. In Seconds
     /// </summary>
