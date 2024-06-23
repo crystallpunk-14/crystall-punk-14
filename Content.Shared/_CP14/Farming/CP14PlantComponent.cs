93033cce2a4defc041e9bb94107cd1c7eede84c7
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-06-23 19:54:01 +0300

    prototype value validating

--- Content.Shared/_CP14/Farming/CP14PlantComponent.cs
+++ Content.Shared/_CP14/Farming/CP14PlantComponent.cs
@@ -9,6 +9,11 @@ namespace Content.Shared._CP14.Farming;
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState(true)]
 public sealed partial class CP14PlantComponent : Component
 {
+    private float _resource;
+    private float _health;
+    private float _energy;
+    private float _growth;
+
     /// <summary>
     /// Soil link. May be null, as not all plants in the world grow on entity soil (e.g. wild shrubs)
     /// </summary>
@@ -18,31 +23,52 @@ public sealed partial class CP14PlantComponent : Component
     /// The ability to consume a resource for growing
     /// </summary>
     [DataField]
-    public float Energy = 0f;
+    public float Energy
+    {
+        set => _energy = MathHelper.Clamp(value, 0, MaxEnergy);
+        get => _energy;
+    }
+
+    [DataField]
+    public float MaxEnergy = 100f;
+
 
     /// <summary>
     /// resource consumed for growth
     /// </summary>
     [DataField]
-    public float Resource = 0f;
+    public float Resource
+    {
+        set => _resource = MathHelper.Clamp(value, 0, MaxResource);
+        get => _resource;
+    }
 
-    /// <summary>
-    /// Plant growth status, 0 to 1
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public float GrowthLevel = 0f;
-
-    /// <summary>
-    ///
-    /// </summary>
     [DataField]
-    public float Health = 1f;
+    public float MaxResource = 100f;
+
+
+    [DataField]
+    public float Health
+    {
+        set => _health = MathHelper.Clamp(value, 0, MaxHealth);
+        get => _health;
+    }
 
     /// <summary>
     ///
     /// </summary>
     [DataField]
     public float MaxHealth = 10f;
+
+    /// <summary>
+    /// Plant growth status, 0 to 1
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public float GrowthLevel
+    {
+        set => _growth = MathHelper.Clamp01(value);
+        get => _growth;
+    }
 
     [DataField(serverOnly: true)]
     public float UpdateFrequency = 90f;
