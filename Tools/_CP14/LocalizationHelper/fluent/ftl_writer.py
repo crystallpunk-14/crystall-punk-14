bf120ec2310db0ebf845c85503e777db5693efdd
comasqw <138010940+comasqw@users.noreply.github.com>
2024-06-24 21:19:37 +0400

    cringe fix

--- Tools/_CP14/LocalizationHelper/fluent/ftl_writer.py
+++ Tools/_CP14/LocalizationHelper/fluent/ftl_writer.py
@@ -7,10 +7,10 @@ def create_ftl(key: str, prototype: dict) -> str:
     ftl += f"ent-{key} = {prototype["name"]}\n"
 
     if prototype["desc"] is not None:
-        ftl += f"   .desc = {prototype["desc"]}\n"
+        ftl += f"    .desc = {prototype["desc"]}\n"
 
     if prototype["suffix"] is not None:
-        ftl += f"   .suffix = {prototype["suffix"]}\n"
+        ftl += f"    .suffix = {prototype["suffix"]}\n"
 
     ftl += "\n"
 
