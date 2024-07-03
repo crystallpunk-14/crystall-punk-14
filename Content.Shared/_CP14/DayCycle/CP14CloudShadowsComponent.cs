d3895e828829b021ae9fc71e5ce3cb38cce35826
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-07-03 15:17:05 +0300

    fix

--- Content.Shared/_CP14/DayCycle/CP14CloudShadowsComponent.cs
+++ Content.Shared/_CP14/DayCycle/CP14CloudShadowsComponent.cs
@@ -1,11 +1,12 @@
 using System.Numerics;
+using Robust.Shared.GameStates;
 
 namespace Content.Shared._CP14.DayCycle;
 
 /// <summary>
 /// if added to the map, renders cloud shadows on the map
 /// </summary>
-[RegisterComponent]
+[RegisterComponent, NetworkedComponent]
 public sealed partial class CP14CloudShadowsComponent : Component
 {
     [DataField]
