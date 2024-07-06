b6d6e2de8c8928f29e337d95fb8670173154a8fa
Ed <edwardxperia2000@gmail.com>
2024-07-06 12:19:32 +0300

    haha!

--- Content.Server/_CP14/BiomeSpawner/CP14BiomeSpawnerComponent.cs
+++ Content.Server/_CP14/BiomeSpawner/CP14BiomeSpawnerComponent.cs
@@ -1,3 +1,12 @@
+/*
+ * All right reserved to CrystallPunk.
+ *
+ * This file is sublicensed under Custom License Agreement for Stalker14 project (https://github.com/stalker14-project/stalker14) only
+ *
+ * See LICENSE.TXT file in the project root for full license information.
+ * Copyright (c) 2024 TheShuEd (Github)
+ */
+
 using Content.Shared.Parallax.Biomes;
 using Robust.Shared.Prototypes;
 
