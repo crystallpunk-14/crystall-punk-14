106a8ae7eaf24398f9f15cdfebafe53af3332a13
Ed <edwardxperia2000@gmail.com>
2024-07-09 00:37:07 +0300

    Update CP14ExpeditionObjectivesRule.cs

--- Content.Server/_CP14/GameTicking/Rules/CP14ExpeditionObjectivesRule.cs
+++ Content.Server/_CP14/GameTicking/Rules/CP14ExpeditionObjectivesRule.cs
@@ -1,28 +1,11 @@
 using Content.Server._CP14.GameTicking.Rules.Components;
-using Content.Server.Antag;
 using Content.Server.Mind;
-using Content.Server.Objectives;
-using Content.Server.Traitor.Uplink;
-using Content.Shared.NPC.Systems;
-using Content.Shared.Players;
-using Content.Shared.Roles;
-using Content.Shared.Roles.Jobs;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Random;
 
 namespace Content.Server.GameTicking.Rules;
 
 public sealed class CP14ExpeditionObjectivesRule : GameRuleSystem<CP14ExpeditionObjectivesRuleComponent>
 {
-    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
-    [Dependency] private readonly IRobustRandom _random = default!;
-    [Dependency] private readonly NpcFactionSystem _npcFaction = default!;
-    [Dependency] private readonly AntagSelectionSystem _antag = default!;
-    [Dependency] private readonly UplinkSystem _uplink = default!;
     [Dependency] private readonly MindSystem _mind = default!;
-    [Dependency] private readonly SharedRoleSystem _role = default!;
-    [Dependency] private readonly SharedJobSystem _jobs = default!;
-    [Dependency] private readonly ObjectivesSystem _objectives = default!;
 
     public override void Initialize()
     {
