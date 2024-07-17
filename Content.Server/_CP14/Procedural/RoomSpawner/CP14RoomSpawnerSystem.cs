7c0c8c5b3348a880f378e1eb24c0fb9ba137d052
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-07-17 17:38:00 +0300

    polishing, bugfixing

--- Content.Server/_CP14/Procedural/RoomSpawner/CP14RoomSpawnerSystem.cs
+++ Content.Server/_CP14/Procedural/RoomSpawner/CP14RoomSpawnerSystem.cs
@@ -13,7 +13,6 @@ public sealed class CP14RoomSpawnerSystem : EntitySystem
     [Dependency] private readonly IPrototypeManager _proto = default!;
     [Dependency] private readonly DungeonSystem _dungeon = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
-    [Dependency] private readonly TransformSystem _transform = default!;
     [Dependency] private readonly SharedMapSystem _maps = default!;
 
     public override void Initialize()
@@ -31,19 +30,23 @@ public sealed class CP14RoomSpawnerSystem : EntitySystem
         var roomProto = rooms.Pick(_random);
 
         if (!_proto.TryIndex<DungeonRoomPrototype>(roomProto, out var room))
+        {
+            Log.Error($"Unable to find matching room prototype ({room}) for {ToPrettyString(spawner)}");
             return;
+        }
 
         var gridUid = Transform(spawner).GridUid;
 
         if (!TryComp<MapGridComponent>(gridUid, out var gridComp))
             return;
 
+        var xform = Transform(spawner).Coordinates.Offset(- room.Size / 2);
         var random = new Random();
 
         _dungeon.SpawnRoom(
             gridUid.Value,
             gridComp,
-            _maps.LocalToTile(gridUid.Value, gridComp, Transform(spawner).Coordinates),
+            _maps.LocalToTile(gridUid.Value, gridComp, xform),
             room,
             random,
             null,
