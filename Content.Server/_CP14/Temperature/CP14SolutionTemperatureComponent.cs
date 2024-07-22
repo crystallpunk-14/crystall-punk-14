5660cd25ee64fb2c6aec74efc558fcdf3cfd92af
Ed <edwardxperia2000@gmail.com>
2024-07-22 22:36:53 +0300

    temp tweak

--- Content.Server/_CP14/Temperature/CP14SolutionTemperatureComponent.cs
+++ Content.Server/_CP14/Temperature/CP14SolutionTemperatureComponent.cs
@@ -3,7 +3,7 @@ namespace Content.Server._CP14.Temperature;
 /// <summary>
 /// passively returns the solution temperature to the standard
 /// </summary>
-[RegisterComponent, Access(typeof(CP14SolutionTemperatureSystem))]
+[RegisterComponent, Access(typeof(CP14TemperatureSystem))]
 public sealed partial class CP14SolutionTemperatureComponent : Component
 {
     [DataField]
