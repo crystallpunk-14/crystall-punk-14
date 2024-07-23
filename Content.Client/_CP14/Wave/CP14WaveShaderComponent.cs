e806655c9d78c0cc026b1a49f20b1eb4c9178ea6
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-23 18:27:09 +1000

    Wave shader WIP  (#288)
    * Created wave shader
    
    * more to _CP14 folders
    
    * add to tree
    
    * fix
    
    * pipiipi
    
    * Provided shader uniform to component fields
    
    * Fixed wave shader shit
    
    * goodies
    
    ---------
    
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>
    Co-authored-by: Ed <edwardxperia2000@gmail.com>

---
+++ Content.Client/_CP14/Wave/CP14WaveShaderComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace Content.Client._CP14.Wave;
+
+[RegisterComponent]
+[Access(typeof(CP14WaveShaderSystem))]
+public sealed partial class CP14WaveShaderComponent : Component
+{
+    [DataField]
+    public float Speed = 10f;
+
+    [DataField]
+    public float Dis = 10f;
+
+    [DataField]
+    public float Offset = 0f;
+}
