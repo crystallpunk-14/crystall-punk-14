18338828cdc0ac93d8578b03a8f0c55b868112ba
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-23 16:54:38 +1000

    Provided shader uniform to component fields

--- Content.Client/_CP14/Wave/CP14WaveShaderComponent.cs
+++ Content.Client/_CP14/Wave/CP14WaveShaderComponent.cs
@@ -4,4 +4,12 @@
 [Access(typeof(CP14WaveShaderSystem))]
 public sealed partial class CP14WaveShaderComponent : Component
 {
+    [DataField]
+    public float Speed = 10f;
+
+    [DataField]
+    public float Dis = 10f;
+
+    [DataField]
+    public float Offset = 0f;
 }
--- Content.Client/_CP14/Wave/CP14WaveShaderSystem.cs
+++ Content.Client/_CP14/Wave/CP14WaveShaderSystem.cs
@@ -17,14 +17,15 @@ public sealed class CP14WaveShaderSystem : EntitySystem
         base.Initialize();
 
         _shader = _protoMan.Index<ShaderPrototype>("Wave").InstanceUnique();
-        _shader.SetParameter("Offset", _random.NextFloat(0, 1000));
 
         SubscribeLocalEvent<CP14WaveShaderComponent, ComponentStartup>(OnStartup);
         SubscribeLocalEvent<CP14WaveShaderComponent, ComponentShutdown>(OnShutdown);
+        SubscribeLocalEvent<CP14WaveShaderComponent, BeforePostShaderRenderEvent>(OnBeforeShaderPost);
     }
 
     private void OnStartup(Entity<CP14WaveShaderComponent> entity, ref ComponentStartup args)
     {
+        entity.Comp.Offset = _random.NextFloat(0, 1000);
         SetShader(entity.Owner, _shader);
     }
 
@@ -39,5 +40,14 @@ public sealed class CP14WaveShaderSystem : EntitySystem
             return;
 
         entity.Comp.PostShader = instance;
+        entity.Comp.GetScreenTexture = instance is not null;
+        entity.Comp.RaiseShaderEvent = instance is not null;
+    }
+
+    private void OnBeforeShaderPost(Entity<CP14WaveShaderComponent> entity, ref BeforePostShaderRenderEvent args)
+    {
+        _shader.SetParameter("Speed", entity.Comp.Speed);
+        _shader.SetParameter("Dis", entity.Comp.Dis);
+        _shader.SetParameter("Offset", entity.Comp.Offset);
     }
 }
