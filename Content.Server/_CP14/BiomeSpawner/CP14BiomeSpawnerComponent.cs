99854edc930e03af26b46fdb10c9ec3e6a9a96ae
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-07-25 10:59:36 +0300

    MIT BiomeSpawner

--- Content.Server/_CP14/BiomeSpawner/CP14BiomeSpawnerComponent.cs
+++ Content.Server/_CP14/BiomeSpawner/CP14BiomeSpawnerComponent.cs
@@ -1,10 +1,8 @@
 /*
  * All right reserved to CrystallPunk.
  *
- * This file is sublicensed under Custom License Agreement for Stalker14 project (https://github.com/stalker14-project/stalker14) only
+ * BUT this file is sublicensed under MIT License
  *
- * See LICENSE.TXT file in the project root for full license information.
- * Copyright (c) 2024 TheShuEd (Github)
  */
 
 using Content.Shared.Parallax.Biomes;
