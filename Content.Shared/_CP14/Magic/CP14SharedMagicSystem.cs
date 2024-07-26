4f47e2300a651ee3e8b597ab6a3ff3c6c10a9dd0
Ed <edwardxperia2000@gmail.com>
2024-07-26 19:34:04 +0300

    delayed projectile spells

--- Content.Shared/_CP14/Magic/CP14SharedMagicSystem.cs
+++ Content.Shared/_CP14/Magic/CP14SharedMagicSystem.cs
@@ -2,6 +2,10 @@ using Content.Shared._CP14.Magic.Components;
 using Content.Shared._CP14.Magic.Events;
 using Content.Shared.DoAfter;
 using Content.Shared.EntityEffects;
+using Content.Shared.Weapons.Ranged.Systems;
+using Robust.Shared.Map;
+using Robust.Shared.Network;
+using Robust.Shared.Physics.Systems;
 
 namespace Content.Shared._CP14.Magic;
 
@@ -10,7 +14,12 @@ namespace Content.Shared._CP14.Magic;
 /// </summary>
 public sealed class CP14SharedMagicSystem : EntitySystem
 {
+    [Dependency] private readonly SharedPhysicsSystem _physics = default!;
     [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly INetManager _net = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly SharedGunSystem _gunSystem = default!;
 
     public override void Initialize()
     {
@@ -18,11 +27,14 @@ public sealed class CP14SharedMagicSystem : EntitySystem
 
         SubscribeLocalEvent<CP14MagicEffectComponent, CP14BeforeCastMagicEffectEvent>(OnBeforeCastMagicEffect);
 
-        SubscribeLocalEvent<CP14DelayedEntityTargetActionEvent>(OnEntityEffectAction);
-        SubscribeLocalEvent<CP14DelayedEntityTargetActionComponent, CP14CastMagicEffectDoAfterEvent>(OnMagicEffectDoAfter);
+        SubscribeLocalEvent<CP14DelayedEntityTargetActionEvent>(OnEntityTargetAction);
+        SubscribeLocalEvent<CP14DelayedEntityTargetActionComponent, CP14DelayedEntityTargetActionDoAfterEvent>(OnCastEntityTarget);
+
+        SubscribeLocalEvent<CP14DelayedWorldTargetActionEvent>(OnWorldTargetAction);
+        SubscribeLocalEvent<CP14DelayedProjectileSpellComponent, CP14DelayedWorldTargetActionDoAfterEvent>(OnCastProjectileSpell);
     }
 
-    private void OnEntityEffectAction(CP14DelayedEntityTargetActionEvent args)
+    private void OnWorldTargetAction(CP14DelayedWorldTargetActionEvent args)
     {
         if (args.Handled)
             return;
@@ -32,7 +44,12 @@ public sealed class CP14SharedMagicSystem : EntitySystem
         if (args is not ICP14DelayedMagicEffect delayedEffect)
             return;
 
-        var doAfterEventArgs = new DoAfterArgs(EntityManager, args.Performer, delayedEffect.Delay, new CP14CastMagicEffectDoAfterEvent(), args.Action, args.Target)
+        var doAfter = new CP14DelayedWorldTargetActionDoAfterEvent()
+        {
+            Target = EntityManager.GetNetCoordinates(args.Target)
+        };
+
+        var doAfterEventArgs = new DoAfterArgs(EntityManager, args.Performer, delayedEffect.Delay, doAfter, args.Action)
         {
             BreakOnMove = delayedEffect.BreakOnMove,
             BreakOnDamage = delayedEffect.BreakOnDamage,
@@ -41,7 +58,26 @@ public sealed class CP14SharedMagicSystem : EntitySystem
         _doAfter.TryStartDoAfter(doAfterEventArgs);
     }
 
-    private void OnMagicEffectDoAfter(Entity<CP14DelayedEntityTargetActionComponent> ent, ref CP14CastMagicEffectDoAfterEvent args)
+    private void OnEntityTargetAction(CP14DelayedEntityTargetActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        args.Handled = true;
+
+        if (args is not ICP14DelayedMagicEffect delayedEffect)
+            return;
+
+        var doAfterEventArgs = new DoAfterArgs(EntityManager, args.Performer, delayedEffect.Delay, new CP14DelayedEntityTargetActionDoAfterEvent(), args.Action, args.Target)
+        {
+            BreakOnMove = delayedEffect.BreakOnMove,
+            BreakOnDamage = delayedEffect.BreakOnDamage,
+        };
+
+        _doAfter.TryStartDoAfter(doAfterEventArgs);
+    }
+
+    private void OnCastEntityTarget(Entity<CP14DelayedEntityTargetActionComponent> ent, ref CP14DelayedEntityTargetActionDoAfterEvent args)
     {
         if (args.Cancelled || args.Handled || args.Target == null)
             return;
@@ -54,8 +90,32 @@ public sealed class CP14SharedMagicSystem : EntitySystem
         }
     }
 
+    private void OnCastProjectileSpell(Entity<CP14DelayedProjectileSpellComponent> spell, ref CP14DelayedWorldTargetActionDoAfterEvent args)
+    {
+        if (args.Cancelled || args.Handled || !_net.IsServer)
+            return;
+
+        args.Handled = true;
+
+        var xform = Transform(args.User);
+        var fromCoords = xform.Coordinates;
+        var toCoords = GetCoordinates(args.Target);
+        var userVelocity = _physics.GetMapLinearVelocity(args.User);
+
+        // If applicable, this ensures the projectile is parented to grid on spawn, instead of the map.
+        var fromMap = fromCoords.ToMap(EntityManager, _transform);
+        var spawnCoords = _mapManager.TryFindGridAt(fromMap, out var gridUid, out _)
+            ? fromCoords.WithEntityId(gridUid, EntityManager)
+            : new(_mapManager.GetMapEntityId(fromMap.MapId), fromMap.Position);
+
+        var ent = Spawn(spell.Comp.Prototype, spawnCoords);
+        var direction = toCoords.ToMapPos(EntityManager, _transform) -
+                        spawnCoords.ToMapPos(EntityManager, _transform);
+        _gunSystem.ShootProjectile(ent, direction, userVelocity, args.User, args.User);
+    }
+
+
     private void OnBeforeCastMagicEffect(Entity<CP14MagicEffectComponent> ent, ref CP14BeforeCastMagicEffectEvent args)
     {
     }
 }
-
--- Content.Shared/_CP14/Magic/Components/CP14DelayedEntityTargetActionComponent.cs
+++ Content.Shared/_CP14/Magic/Components/CP14DelayedEntityTargetActionComponent.cs
@@ -14,20 +14,3 @@ public sealed partial class CP14DelayedEntityTargetActionComponent : Component
     [DataField(required: true, serverOnly: true)]
     public List<EntityEffect> Effects = new();
 }
-
-public sealed partial class CP14DelayedEntityTargetActionEvent : EntityTargetActionEvent, ICP14DelayedMagicEffect
-{
-    [DataField]
-    public float Delay { get; private set; } = 1f;
-
-    [DataField]
-    public bool BreakOnMove { get; private set; } = true;
-
-    [DataField]
-    public bool BreakOnDamage { get; private set; } = true;
-}
-
-[Serializable, NetSerializable]
-public sealed partial class CP14CastMagicEffectDoAfterEvent : SimpleDoAfterEvent
-{
-}
