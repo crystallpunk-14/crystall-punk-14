5f63c106223aa3e7776b4a5178c569caf81d7e94
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-07-31 15:29:49 +1000

    Fixed serialization errors

--- Content.Shared/_CP14/Workbench/CP14WorkbenchUI.cs
+++ Content.Shared/_CP14/Workbench/CP14WorkbenchUI.cs
@@ -1,5 +1,8 @@
-﻿namespace Content.Shared._CP14.Workbench;
+﻿using Robust.Shared.Serialization;
 
+namespace Content.Shared._CP14.Workbench;
+
+[Serializable, NetSerializable]
 public enum CP14WorkbenchUiKey
 {
     Key,
