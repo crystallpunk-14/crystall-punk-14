0cd3a3c2d9812ed5b4f3904ba16efbf3a91577bb
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-04 18:48:09 +1000

    Added personal signature system (#382)
    * Added personal signature system
    
    * FUCKING RUSSIAN LANUAGE
    
    A HATE LATTERS, HATEEEEEEEEEEEEEEEEEEEEEEE!
    
    * Added locale and sound

---
+++ Content.Server/_CP14/PersonalSignature/CP14PersonalSignatureComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.Audio;
+
+namespace Content.Server._CP14.PersonalSignature;
+
+[RegisterComponent]
+public sealed partial class CP14PersonalSignatureComponent : Component
+{
+    [DataField]
+    public SoundSpecifier? SignSound;
+}
