f961e48b4887afca6a03773323f5bb366797410e
Ed <96445749+TheShuEd@users.noreply.github.com>
2024-08-06 20:25:17 +0300

    Revert "SpawnSpell tweak (#393)"
    This reverts commit dc853b6662cdb709e7031895fd9f461d1b113ea6.

---
+++ Content.Shared/_CP14/MagicSpell/Spells/CP14SpellSpawnEntityOnUser.cs
@@ -0,0 +1,21 @@
+using Robust.Shared.Map;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CP14.MagicSpell.Spells;
+
+public sealed partial class CP14SpellSpawnEntityOnUser : CP14SpellEffect
+{
+    [DataField]
+    public List<EntProtoId> Spawns = new();
+
+    public override void Effect(EntityManager entManager, CP14SpellEffectBaseArgs args)
+    {
+        if (args.User is null || !entManager.TryGetComponent<TransformComponent>(args.User.Value, out var transformComponent))
+            return;
+
+        foreach (var spawn in Spawns)
+        {
+            entManager.SpawnAtPosition(spawn, transformComponent.Coordinates);
+        }
+    }
+}
