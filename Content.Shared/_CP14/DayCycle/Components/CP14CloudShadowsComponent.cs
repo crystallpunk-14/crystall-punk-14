9f4932a8af9d61ac810a7b8fd94a222fa3901bdf
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2024-08-10 08:12:13 +1000

    Remove day cycle shit (#400)

--- Content.Shared/_CP14/DayCycle/Components/CP14CloudShadowsComponent.cs
+++ Content.Shared/_CP14/DayCycle/Components/CP14CloudShadowsComponent.cs
@@ -1,16 +1,17 @@
 using System.Numerics;
 using Robust.Shared.GameStates;
+using Robust.Shared.Utility;
 
-namespace Content.Shared._CP14.DayCycle;
+namespace Content.Shared._CP14.DayCycle.Components;
 
 /// <summary>
-/// if added to the map, renders cloud shadows on the map
+/// If added to the map, renders cloud shadows on the map
 /// </summary>
 [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
 public sealed partial class CP14CloudShadowsComponent : Component
 {
     [DataField, AutoNetworkedField]
-    public Vector2 CloudSpeed = new Vector2(0.5f, 0f);
+    public Vector2 CloudSpeed = new(0.5f, 0f);
 
     [DataField]
     public float MaxSpeed = 1.5f;
@@ -22,5 +23,5 @@ public sealed partial class CP14CloudShadowsComponent : Component
     public float Scale = 2.5f;
 
     [DataField]
-    public string ParallaxPath = "/Textures/_CP14/Parallaxes/Shadows.png";
+    public ResPath ParallaxPath = new("/Textures/_CP14/Parallaxes/Shadows.png");
 }
