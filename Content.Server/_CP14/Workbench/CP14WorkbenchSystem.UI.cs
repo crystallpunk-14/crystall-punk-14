ccb87bdf306acd4d893de316aad93e6fbc695895
Ed <edwardxperia2000@gmail.com>
2024-08-10 13:37:11 +0300

    workbench + doll sublicense

--- Content.Server/_CP14/Workbench/CP14WorkbenchSystem.UI.cs
+++ Content.Server/_CP14/Workbench/CP14WorkbenchSystem.UI.cs
@@ -1,3 +1,12 @@
+/*
+ * All right reserved to Crystallpunk.
+ *
+ * BUT this file is sublicensed under CLA License for next projects:
+ * https://github.com/technologists-team/after-rain
+ * https://github.com/EscapeD-14/EscapeD-14
+ *
+ */
+
 using Content.Shared._CP14.Workbench;
 
 namespace Content.Server._CP14.Workbench;
--- Content.Server/_CP14/Workbench/CP14WorkbenchSystem.cs
+++ Content.Server/_CP14/Workbench/CP14WorkbenchSystem.cs
@@ -1,3 +1,12 @@
+/*
+ * All right reserved to Crystallpunk.
+ *
+ * BUT this file is sublicensed under CLA License for next projects:
+ * https://github.com/technologists-team/after-rain
+ * https://github.com/EscapeD-14/EscapeD-14
+ *
+ */
+
 using Content.Server.DoAfter;
 using Content.Server.Popups;
 using Content.Server.Stack;
