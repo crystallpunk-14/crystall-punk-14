945a0ec5fa5fda1dcaa31cd01a78c5994b42f63a
Ed <edwardxperia2000@gmail.com>
2024-08-27 16:05:24 +0300

    fix startup ordering

--- Content.Client/_CP14/Wallpaper/CP14ClientWallpaperSystem.cs
+++ Content.Client/_CP14/Wallpaper/CP14ClientWallpaperSystem.cs
@@ -1,4 +1,6 @@
+using Content.Client.IconSmoothing;
 using Content.Shared._CP14.Wallpaper;
+using Content.Shared.IconSmoothing;
 using Robust.Client.GameObjects;
 
 namespace Content.Client._CP14.Wallpaper;
@@ -7,8 +9,16 @@ public sealed class CP14ClientWallpaperSystem : CP14SharedWallpaperSystem
 {
     public override void Initialize()
     {
+        SubscribeLocalEvent<CP14WallpaperHolderComponent, AfterAutoHandleStateEvent>(OnHandleState, after: new[] { typeof(IconSmoothSystem) });
+        SubscribeLocalEvent<CP14WallpaperHolderComponent, ComponentStartup>(OnStartup, after: new[] { typeof(IconSmoothSystem) });
+    }
 
-        SubscribeLocalEvent<CP14WallpaperHolderComponent, AfterAutoHandleStateEvent>(OnHandleState);
+    private void OnStartup(Entity<CP14WallpaperHolderComponent> holder, ref ComponentStartup args)
+    {
+        if (!TryComp<SpriteComponent>(holder, out var sprite))
+            return;
+
+        UpdateVisuals(holder, sprite);
     }
 
     private void OnHandleState(Entity<CP14WallpaperHolderComponent> holder, ref AfterAutoHandleStateEvent args)
@@ -16,11 +26,17 @@ public sealed class CP14ClientWallpaperSystem : CP14SharedWallpaperSystem
         if (!TryComp<SpriteComponent>(holder, out var sprite))
             return;
 
+        UpdateVisuals(holder, sprite);
+    }
+
+    private static void UpdateVisuals(Entity<CP14WallpaperHolderComponent> holder, SpriteComponent sprite)
+    {
         //Remove old layers
         foreach (var key in holder.Comp.RevealedLayers)
         {
             sprite.RemoveLayer(key);
         }
+
         holder.Comp.RevealedLayers.Clear();
 
         //Add new layers
