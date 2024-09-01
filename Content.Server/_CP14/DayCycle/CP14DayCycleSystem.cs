8f035c81b6a3c5f2f92b036a19cf8eff88d11519
Ed <edwardxperia2000@gmail.com>
2024-09-01 15:32:57 +0300

    Update CP14DayCycleSystem.cs

--- Content.Server/_CP14/DayCycle/CP14DayCycleSystem.cs
+++ Content.Server/_CP14/DayCycle/CP14DayCycleSystem.cs
@@ -1,7 +1,6 @@
 using Content.Shared._CP14.DayCycle;
 using Content.Shared._CP14.DayCycle.Components;
 using Robust.Shared.Map.Components;
-using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
@@ -13,8 +12,6 @@ public sealed partial class CP14DayCycleSystem : CP14SharedDayCycleSystem
     private const float MaxTimeDiff = 0.05f;
 
     [Dependency] private readonly IGameTiming _timing = default!;
-    [Dependency] private readonly SharedMapSystem _maps = default!;
-    [Dependency] private readonly ITileDefinitionManager _tileDefManager = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
 
     public override void Initialize()
@@ -119,4 +116,3 @@ public sealed partial class CP14DayCycleSystem : CP14SharedDayCycleSystem
         return MathHelper.Clamp01((float)(distanceFromStart / totalDistance));
     }
 }
-
