2fc198d61ca7a86d0c7d538db82e78b2e3c68d67
Ed <edwardxperia2000@gmail.com>
2024-10-26 16:58:19 +0300

    finish vault markers

--- Content.Server/_CP14/StealArea/CP14StealAreaAutoJobConnectSystem.cs
+++ Content.Server/_CP14/StealArea/CP14StealAreaAutoJobConnectSystem.cs
@@ -1,6 +1,8 @@
 using Content.Server.GameTicking;
 using Content.Server.Mind;
 using Content.Server.Objectives.Components;
+using Content.Shared.Roles.Jobs;
+using Robust.Server.Player;
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._CP14.StealArea;
@@ -10,11 +12,50 @@ public sealed class CP14StealAreaAutoJobConnectSystem : EntitySystem
 
     [Dependency] private readonly MindSystem _mind = default!;
     [Dependency] private readonly IPrototypeManager _proto = default!;
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly SharedJobSystem _job = default!;
     public override void Initialize()
     {
         base.Initialize();
 
+        SubscribeLocalEvent<CP14StealAreaAutoJobConnectComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<PlayerSpawnCompleteEvent>(OnPlayerSpawning);
+    }
+
+    private void OnMapInit(Entity<CP14StealAreaAutoJobConnectComponent> autoConnect, ref MapInitEvent args)
+    {
+        if (!TryComp<StealAreaComponent>(autoConnect, out var stealArea))
+            return;
+
+        foreach (var player in _playerManager.Sessions)
+        {
+            if (!_mind.TryGetMind(player.UserId, out var playerMind))
+                continue;
+
+            if (!_job.MindTryGetJob(playerMind, out var playerJob))
+                continue;
+
+            if (stealArea.Owners.Contains(playerMind.Value))
+                continue;
+
+            if (autoConnect.Comp.Jobs.Contains(playerJob))
+            {
+                stealArea.Owners.Add(playerMind.Value);
+                break;
+            }
+
+            foreach (var depObj in autoConnect.Comp.Departments)
+            {
+                if (!_proto.TryIndex(depObj, out var indexedDepart))
+                    continue;
+
+                if (!indexedDepart.Roles.Contains(playerJob))
+                    continue;
+
+                stealArea.Owners.Add(playerMind.Value);
+                break;
+            }
+        }
     }
 
     private void OnPlayerSpawning(PlayerSpawnCompleteEvent ev)
