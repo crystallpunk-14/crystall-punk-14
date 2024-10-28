55ef5c8cc88fe515df8e6e7c48e6c853ca10ff57
Ed <edwardxperia2000@gmail.com>
2024-10-28 20:21:15 +0300

    Revert "more experiments"
    This reverts commit a7f30fd6084694e73b951e998a380651a6754942.

--- Content.Client/Parallax/ParallaxOverlay.cs
+++ Content.Client/Parallax/ParallaxOverlay.cs
@@ -32,7 +32,7 @@ public sealed class ParallaxOverlay : Overlay
 
     protected override bool BeforeDraw(in OverlayDrawArgs args)
     {
-        if (args.MapId == MapId.Nullspace /*|| _entManager.HasComponent<BiomeComponent>(_mapManager.GetMapEntityId(args.MapId))*/) //CP14 parallax with biomes
+        if (args.MapId == MapId.Nullspace || _entManager.HasComponent<BiomeComponent>(_mapManager.GetMapEntityId(args.MapId)))
             return false;
 
         return true;
