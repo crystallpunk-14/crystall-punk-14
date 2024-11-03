95abd03765088f663df6c1311e1b1e85c58b4480
Ed <edwardxperia2000@gmail.com>
2024-11-03 21:48:04 +0300

    magic lantern

---
+++ Content.Shared/_CP14/MagicLantern/CP14MagicLanternComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._CP14.MagicLantern;
+
+/// <summary>
+/// Controls the object's glow based on magical energy
+/// </summary>
+[RegisterComponent, Access(typeof(CP14MagicLanternSystem))]
+public sealed partial class CP14MagicLanternComponent : Component
+{
+
+}
