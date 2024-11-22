bc76295ca13035fca3d7bcd7086a4f3a6904dc16
comasqw <arizona.rp.91@mail.ru>
2024-11-22 23:58:20 +0400

    follow on use inhand

---
+++ Content.Shared/_CP14/Follower/CP14FollowOnUseInHandComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._CP14.Follower;
+
+[RegisterComponent, Access(typeof(CP14FollowOnUseInHandSystem))]
+public sealed partial class CP14FollowOnUseInHandComponent : Component
+{
+
+}
