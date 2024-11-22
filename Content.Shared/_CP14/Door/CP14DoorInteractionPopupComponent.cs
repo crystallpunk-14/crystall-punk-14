1a2e32f234e24d62cccb460b4caee430090c6f83
comasqw <arizona.rp.91@mail.ru>
2024-11-22 13:15:51 +0400

    removed old comment

--- Content.Shared/_CP14/Door/CP14DoorInteractionPopupComponent.cs
+++ Content.Shared/_CP14/Door/CP14DoorInteractionPopupComponent.cs
@@ -12,10 +12,6 @@ public sealed partial class CP14DoorInteractionPopupComponent : Component
     [ViewVariables(VVAccess.ReadWrite)]
     public TimeSpan InteractDelay = TimeSpan.FromSeconds(1.0);
 
-    /// <summary>
-    /// String will be used to fetch the localized message to be played if the interaction succeeds.
-    /// Nullable in case none is specified on the yaml prototype.
-    /// </summary>
     [DataField("interactString")]
     public string InteractString = "cp14-closed-door-interact-popup";
 
