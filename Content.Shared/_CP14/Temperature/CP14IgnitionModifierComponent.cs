f1fc16c6f0336e8ce4932307fe51b840356bf6a1
Ed <96445749+TheShuEd@users.noreply.github.com>
2025-01-09 15:27:28 +0300

    Update CP14IgnitionModifierComponent.cs

--- Content.Shared/_CP14/Temperature/CP14IgnitionModifierComponent.cs
+++ Content.Shared/_CP14/Temperature/CP14IgnitionModifierComponent.cs
@@ -1,14 +1,20 @@
 namespace Content.Shared._CP14.Temperature;
 
 /// <summary>
-/// Modifies the burning of this essence.
+/// Modifies how the entity is set on fire. Given that in cp14 the ignition is done via doAfter by a separate add-on
 /// </summary>
 [RegisterComponent, Access(typeof(CP14SharedFireSpreadSystem))]
 public sealed partial class CP14IgnitionModifierComponent : Component
 {
+    /// <summary>
+    /// Allows you to slow down or speed up doAfter the burning of this entity. (Example: candles are lit quickly, they should have a high value)
+    /// </summary>
     [DataField]
     public float IgnitionTimeModifier = 1f;
 
+    /// <summary>
+    /// Should burning this entity warn other players? Put false if it is safe to ignite this entity.
+    /// </summary>
     [DataField]
     public bool HideCaution = false;
 }
