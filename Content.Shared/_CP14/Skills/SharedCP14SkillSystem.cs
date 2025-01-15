62b04706a45dba505c9fba5a4ca9f0d9103ae65e
Ed <96445749+TheShuEd@users.noreply.github.com>
2025-01-15 12:13:42 +0300

    remove all requirements

--- Content.Shared/_CP14/Skills/SharedCP14SkillSystem.cs
+++ Content.Shared/_CP14/Skills/SharedCP14SkillSystem.cs
@@ -33,41 +33,6 @@ public partial class SharedCP14SkillSystem : EntitySystem
         }
     }
 
-    public bool HasEnoughSkillToUse(EntityUid user, EntityUid target, out List<ProtoId<CP14SkillPrototype>> missingSkills)
-    {
-        missingSkills = new();
-        if (!TryComp<CP14SkillRequirementComponent>(target, out var requirement) || requirement.RequiredSkills.Count == 0)
-            return true;
-
-        if (!TryComp<CP14SkillsStorageComponent>(user, out var skillStorage))
-        {
-            missingSkills = requirement.RequiredSkills;
-            return false;
-        }
-
-        var success = requirement.NeedAll;
-        foreach (var skill in requirement.RequiredSkills)
-        {
-            var hasSkill = skillStorage.Skills.Contains(skill);
-
-            if (requirement.NeedAll && !hasSkill)
-            {
-                missingSkills.Add(skill);
-                success = false;
-            }
-            else if (!requirement.NeedAll && hasSkill)
-            {
-                success = true;
-            }
-            else if (!requirement.NeedAll && !hasSkill)
-            {
-                missingSkills.Add(skill);
-            }
-        }
-
-        return success;
-    }
-
     public bool TryLearnSkill(EntityUid uid, ProtoId<CP14SkillPrototype> skill, bool force = false)
     {
         if (!TryComp<CP14SkillsStorageComponent>(uid, out var skillStorage))
