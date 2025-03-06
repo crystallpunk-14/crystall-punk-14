cd59c100f7425657fd2879daf24dd917aee9b73f
Ed <edwardxperia2000@gmail.com>
2025-03-06 22:18:11 +0300

    death under sun

--- Content.Server/_CP14/GameTicking/Rules/CP14VampireRuleSystem.cs
+++ Content.Server/_CP14/GameTicking/Rules/CP14VampireRuleSystem.cs
@@ -1,8 +1,15 @@
+using Content.Server._CP14.DayCycle;
 using Content.Server._CP14.GameTicking.Rules.Components;
 using Content.Server._CP14.Vampire;
+using Content.Server.Atmos.Components;
+using Content.Server.Atmos.EntitySystems;
 using Content.Server.Body.Systems;
 using Content.Server.GameTicking.Rules;
+using Content.Server.Temperature.Components;
+using Content.Server.Temperature.Systems;
 using Content.Shared.Damage;
+using Content.Shared.Popups;
+using Robust.Shared.Timing;
 
 namespace Content.Server._CP14.GameTicking.Rules;
 
@@ -10,6 +17,11 @@ public sealed class CP14VampireRuleSystem : GameRuleSystem<CP14VampireRuleCompon
 {
     [Dependency] private readonly BloodstreamSystem _bloodstream = default!;
     [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+    [Dependency] private readonly TemperatureSystem _temperature = default!;
+    [Dependency] private readonly SharedPopupSystem _popup = default!;
+    [Dependency] private readonly CP14DayCycleSystem _dayCycle = default!;
+    [Dependency] private readonly FlammableSystem _flammable = default!;
 
     public override void Initialize()
     {
@@ -17,6 +29,33 @@ public sealed class CP14VampireRuleSystem : GameRuleSystem<CP14VampireRuleCompon
 
         SubscribeLocalEvent<CP14VampireComponent, MapInitEvent>(OnMapInit);
     }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+
+        var query = EntityQueryEnumerator<CP14VampireComponent, TemperatureComponent, FlammableComponent>();
+        while (query.MoveNext(out var uid, out var vampire, out var temperature, out var flammable))
+        {
+            if (_timing.CurTime < vampire.NextHeatTime)
+                continue;
+
+            vampire.NextHeatTime = _timing.CurTime + vampire.HeatFrequency;
+
+            if (!_dayCycle.TryDaylightThere(uid))
+                continue;
+
+            _temperature.ChangeHeat(uid, vampire.HeatUnderSunTemperature);
+            _popup.PopupEntity(Loc.GetString("cp14-heat-under-sun"), uid, uid, PopupType.SmallCaution);
+
+            if (temperature.CurrentTemperature > vampire.IgniteThreshold && !flammable.OnFire)
+            {
+                _flammable.AdjustFireStacks(uid, 1, flammable);
+                _flammable.Ignite(uid, uid, flammable);
+            }
+        }
+    }
+
 
     private void OnMapInit(Entity<CP14VampireComponent> ent, ref MapInitEvent args)
     {
--- Content.Server/_CP14/Vampire/CP14VampireComponent.cs
+++ Content.Server/_CP14/Vampire/CP14VampireComponent.cs
@@ -10,4 +10,16 @@ public sealed partial class CP14VampireComponent : Component
 {
     [DataField]
     public ProtoId<ReagentPrototype> NewBloodReagent = "CP14BloodVampire";
+
+    [DataField]
+    public float HeatUnderSunTemperature = 12000f;
+
+    [DataField]
+    public TimeSpan HeatFrequency = TimeSpan.FromSeconds(1);
+
+    [DataField]
+    public TimeSpan NextHeatTime = TimeSpan.Zero;
+
+    [DataField]
+    public float IgniteThreshold = 350f;
 }
