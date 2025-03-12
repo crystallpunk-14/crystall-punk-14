fe8b4121ad8179964e32b2bcde4c4b01d442cdff
Ed <edwardxperia2000@gmail.com>
2025-03-12 14:36:34 +0300

    demiplane hints

--- Content.Shared/_CP14/Demiplane/CP14SharedDemiplanSystem.cs
+++ Content.Shared/_CP14/Demiplane/CP14SharedDemiplanSystem.cs
@@ -2,7 +2,6 @@ using Content.Shared._CP14.Demiplane.Components;
 using Content.Shared._CP14.DemiplaneTraveling;
 using Content.Shared.DoAfter;
 using Content.Shared.Interaction;
-using Robust.Shared.Player;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._CP14.Demiplane;
@@ -10,11 +9,36 @@ namespace Content.Shared._CP14.Demiplane;
 public abstract partial  class CP14SharedDemiplaneSystem : EntitySystem
 {
     [Dependency] private readonly SharedDoAfterSystem _doAfter = default!;
+    [Dependency] private readonly EntityLookupSystem _lookup = default!;
+    [Dependency] private readonly SharedTransformSystem _transform = default!;
     public override void Initialize()
     {
         base.Initialize();
 
         SubscribeLocalEvent<CP14DemiplaneRiftOpenedComponent, InteractHandEvent>(OnDemiplanePasswayInteract);
+        SubscribeLocalEvent<CP14DemiplaneHintComponent, MapInitEvent>(OnDemiplaneHintMapInit);
+    }
+
+    private void OnDemiplaneHintMapInit(Entity<CP14DemiplaneHintComponent> ent, ref MapInitEvent args)
+    {
+        var query = EntityQueryEnumerator<CP14DemiplaneRiftOpenedComponent, TransformComponent>();
+
+        var xformHint = Transform(ent);
+        var hintPos = _transform.GetWorldPosition(xformHint);
+
+        while (query.MoveNext(out var uid, out _, out var xformRift))
+        {
+            if (xformRift.MapUid != xformHint.MapUid)
+                continue;
+
+            var riftPos = _transform.GetWorldPosition(xformRift);
+
+            //Calculate the rotation
+            Angle angle = new(riftPos - hintPos);
+
+            _transform.SetWorldRotation(ent, angle + Angle.FromDegrees(90));
+            break;
+        }
     }
 
     private void OnDemiplanePasswayInteract(Entity<CP14DemiplaneRiftOpenedComponent> passway, ref InteractHandEvent args)
