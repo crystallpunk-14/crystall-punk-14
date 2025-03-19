709b78080fbc523bf3c452fb847b052cffa48899
Арт <123451459+JustArt1m@users.noreply.github.com>
2025-03-19 13:36:46 +0300

    Weapon Sharpening System Update & Bread Slicing Fix (#915)
    * SharpeningUpdate
    
    * BreadSlicingFix
    
    Before that, bread slices could be cut endlessly into new bread slices.
    
    ---------
    
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>

--- Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
+++ Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
@@ -39,9 +39,15 @@ namespace Content.Server.Damage.Systems
             {
                 //CrystallEdge Melee upgrade
                 var damage = component.Damage;
+                var slashDamage = damage.DamageDict.GetValueOrDefault("Slash");
+                var piercingDamage = damage.DamageDict.GetValueOrDefault("Piercing");
 
                 if (TryComp<CP14SharpenedComponent>(uid, out var sharp))
-                    damage *= sharp.Sharpness;
+                {
+                    damage.DamageDict["Slash"] = slashDamage * sharp.Sharpness;
+                    damage.DamageDict["Piercing"] = piercingDamage * sharp.Sharpness;
+                    damage.DamageDict["Blunt"] = (slashDamage + piercingDamage) / 2 * (1f - sharp.Sharpness);
+                }
 
                 var dmg = _damageable.TryChangeDamage(args.Target, damage * _damageable.UniversalThrownDamageModifier, component.IgnoreResistances, origin: args.Component.Thrower);
                 //CrystallEdge Melee upgrade end
@@ -66,11 +72,16 @@ namespace Content.Server.Damage.Systems
 
         private void OnDamageExamine(EntityUid uid, DamageOtherOnHitComponent component, ref DamageExamineEvent args)
         {
-            //CP14 Sharpening damage apply
             var damage = component.Damage * _damageable.UniversalThrownDamageModifier;
+            var slashDamage = damage.DamageDict.GetValueOrDefault("Slash");
+            var piercingDamage = damage.DamageDict.GetValueOrDefault("Piercing");
 
             if (TryComp<CP14SharpenedComponent>(uid, out var sharp))
-                damage *= sharp.Sharpness;
+            {
+                damage.DamageDict["Slash"] = slashDamage * sharp.Sharpness;
+                damage.DamageDict["Piercing"] = piercingDamage * sharp.Sharpness;
+                damage.DamageDict["Blunt"] = (slashDamage + piercingDamage) / 2 * (1f - sharp.Sharpness);
+            }
 
             _damageExamine.AddDamageExamine(args.Message, damage, Loc.GetString("damage-throw"));
             //CP14 Sharpening damage apply end
