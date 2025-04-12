7ebb40fa97281129ab75bb67ad16d7bb6e36a23d
Ed <edwardxperia2000@gmail.com>
2025-04-12 15:41:15 +0300

    wendigo

--- Content.Server/_CP14/MagicSpell/CP14MagicSystem.cs
+++ Content.Server/_CP14/MagicSpell/CP14MagicSystem.cs
@@ -37,7 +37,7 @@ public sealed partial class CP14MagicSystem : CP14SharedMagicSystem
         SubscribeLocalEvent<CP14SpellEffectOnHitComponent, MeleeHitEvent>(OnMeleeHit);
         SubscribeLocalEvent<CP14SpellEffectOnHitComponent, ThrowDoHitEvent>(OnProjectileHit);
 
-        SubscribeLocalEvent<CP14MagicEffectVerbalAspectComponent, CP14VerbalAspectSpeechEvent>(OnSpellSpoken);
+        SubscribeLocalEvent<CP14MagicEffectVerbalAspectComponent, CP14SpellSpeechEvent>(OnSpellSpoken);
 
         SubscribeLocalEvent<CP14MagicEffectCastingVisualComponent, CP14StartCastMagicEffectEvent>(OnSpawnMagicVisualEffect);
         SubscribeLocalEvent<CP14MagicEffectCastingVisualComponent, CP14EndCastMagicEffectEvent>(OnDespawnMagicVisualEffect);
@@ -100,7 +100,7 @@ public sealed partial class CP14MagicSystem : CP14SharedMagicSystem
         }
     }
 
-    private void OnSpellSpoken(Entity<CP14MagicEffectVerbalAspectComponent> ent, ref CP14VerbalAspectSpeechEvent args)
+    private void OnSpellSpoken(Entity<CP14MagicEffectVerbalAspectComponent> ent, ref CP14SpellSpeechEvent args)
     {
         if (args.Performer is not null && args.Speech is not null)
             _chat.TrySendInGameICMessage(args.Performer.Value, args.Speech, args.Emote ? InGameICChatType.Emote : InGameICChatType.Speak, true);
--- Resources/Locale/ru-RU/_CP14/ghostRoles/roles.ftl
+++ Resources/Locale/ru-RU/_CP14/ghostRoles/roles.ftl
@@ -1,2 +1,5 @@
 cp14-ghost-role-information-name-skeleton = Оживленный скелет
 cp14-ghost-role-information-description-skeleton = Порождение некромантии, вселившее живую душу в мертвые кости. Часто такие души контролируются некромантом, исполняя его волю.
+
+cp14-ghost-role-information-name-wendigo = Вендиго
+cp14-ghost-role-information-description-wendigo = Дух ночи и голода, охотящийся за одиночками, отделившихся от своих товарищей.
