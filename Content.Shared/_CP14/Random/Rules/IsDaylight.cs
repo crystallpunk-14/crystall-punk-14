71527bb4eb171d50481eb6a5489b43da8e6aaf70
Red <96445749+TheShuEd@users.noreply.github.com>
2025-05-31 14:31:44 +0300

    Merge pull request #1341 from crystallpunk-14/ed-31-05-2025-web
    Spider content update

--- Content.Shared/_CP14/Random/Rules/IsDaylight.cs
+++ Content.Shared/_CP14/Random/Rules/IsDaylight.cs
@@ -1,27 +1,25 @@
+using Content.Shared._CP14.DayCycle;
 using Content.Shared.Random.Rules;
-using Robust.Shared.Map.Components;
 
 namespace Content.Shared._CP14.Random.Rules;
 
 /// <summary>
 /// Checks whether there is a time of day on the current map, and whether the current time of day corresponds to the specified periods.
 /// </summary>
-public sealed partial class CP14TimePeriod : RulesRule
+public sealed partial class CP14IsNight : RulesRule
 {
-    [DataField]
-    public float Threshold = 100f;
-
     public override bool Check(EntityManager entManager, EntityUid uid)
     {
         var transform = entManager.System<SharedTransformSystem>();
+        var dayCycle = entManager.System<CP14DayCycleSystem>();
 
         var map = transform.GetMap(uid);
-        if (!entManager.TryGetComponent<MapLightComponent>(map, out var light))
+
+        if (map is null)
             return false;
 
-        var lightColor = light.AmbientLightColor;
-        var medium = lightColor.R + lightColor.G + lightColor.B / 3f;
+        var lightLevel = dayCycle.GetLightLevel(map.Value);
 
-        return medium > Threshold;
+        return Inverted ? lightLevel < 0.5 : lightLevel >= 0.5;
     }
 }
