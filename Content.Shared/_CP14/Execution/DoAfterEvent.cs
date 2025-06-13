5c7f4ae7a9d9732c1463d0ac23a24faf3fc3f8c5
Sefaia <sefaia.kitsune@gmail.com>
2025-06-13 07:11:07 -0500

    Removed redundant code and replace CP14Execution Component with original Execution Component. Removed Mace, Hammer, Hoe, Shovel and Pickaxe execution for now. Added Execution Component to BaseWeaponSharp modifier

--- Content.Shared/_CP14/Execution/DoAfterEvent.cs
+++
@@ -1,9 +0,0 @@
-using Content.Shared.DoAfter;
-using Robust.Shared.Serialization;
-
-namespace Content.Shared._CP14.Execution;
-
-[Serializable, NetSerializable]
-public sealed partial class ExecutionDoAfterEvent : SimpleDoAfterEvent
-{
-}
