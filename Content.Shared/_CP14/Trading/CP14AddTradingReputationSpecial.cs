8ddb65942b2ed9d78baf64ed1381f0a94896859a
Ed <edwardxperia2000@gmail.com>
2025-07-06 17:21:06 +0300

    Refactor trading reputation special and update guard assets
    Renamed CP14AddGlobalReputationSpecial to CP14AddTradingReputationSpecial and updated its logic to use a configurable set of factions. Updated job prototypes for guard, guard commander, investigator, and merchant to use the new special and specify relevant factions. Also updated guard helmet textures and metadata.

--- Content.Shared/_CP14/Trading/CP14AddTradingReputationSpecial.cs
+++ Content.Shared/_CP14/Trading/CP14AddTradingReputationSpecial.cs
@@ -5,18 +5,20 @@ using Robust.Shared.Prototypes;
 
 namespace Content.Shared._CP14.Trading;
 
-public sealed partial class CP14AddGlobalReputationSpecial : JobSpecial
+public sealed partial class CP14AddTradingReputationSpecial : JobSpecial
 {
     [DataField]
     public float Reputation = 1f;
 
+    [DataField]
+    public HashSet<ProtoId<CP14TradingFactionPrototype>> Factions = new();
+
     public override void AfterEquip(EntityUid mob)
     {
         var entMan = IoCManager.Resolve<IEntityManager>();
-        var protoMan = IoCManager.Resolve<IPrototypeManager>();
         var tradeSys = entMan.System<CP14SharedTradingPlatformSystem>();
 
-        foreach (var faction in protoMan.EnumeratePrototypes<CP14TradingFactionPrototype>())
+        foreach (var faction in Factions)
         {
             tradeSys.AddReputation(mob, faction, Reputation);
         }
