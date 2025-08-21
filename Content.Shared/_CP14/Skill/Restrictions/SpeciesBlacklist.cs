4a5bc15bd85f9dbcf2beef8247db18c7e6baabd7
Viator-MV <egor44148@gmail.com>
2025-08-21 16:03:57 +0300

    Good fire skele (#1680)
    * Revert "fire magic bad skele (#1679)"
    
    This reverts commit b130d8aeec1a3d0de09ea842994ec8c6dcd4798d.
    
    * skelehehehe
    
    * kit conversation

---
+++ Content.Shared/_CP14/Skill/Restrictions/SpeciesBlacklist.cs
@@ -0,0 +1,27 @@
+using Content.Shared._CP14.Skill.Prototypes;
+using Content.Shared.Humanoid;
+using Content.Shared.Humanoid.Prototypes;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CP14.Skill.Restrictions;
+
+public sealed partial class SpeciesBlacklist : CP14SkillRestriction
+{
+    [DataField(required: true)]
+    public ProtoId<SpeciesPrototype> Species = new();
+
+    public override bool Check(IEntityManager entManager, EntityUid target, CP14SkillPrototype skill)
+    {
+        if (!entManager.TryGetComponent<HumanoidAppearanceComponent>(target, out var appearance))
+            return false;
+
+        return appearance.Species != Species;
+    }
+
+    public override string GetDescription(IEntityManager entManager, IPrototypeManager protoManager)
+    {
+        var species = protoManager.Index(Species);
+
+        return Loc.GetString("cp14-skill-req-notspecies", ("name", Loc.GetString(species.Name)));
+    }
+}
--- Resources/Locale/ru-RU/_CP14/skill/requirements.ftl
+++ Resources/Locale/ru-RU/_CP14/skill/requirements.ftl
@@ -1,5 +1,6 @@
 cp14-skill-req-prerequisite = Навык "{$name}" должен быть изучен
 cp14-skill-req-species = Вы должны быть расы "{$name}"
+cp14-skill-req-notspecies = Вы не должны быть расы "{$name}"
 cp14-skill-req-researched = Необходимо провести исследование на исследовательском столе
 cp14-skill-req-impossible = Невозможно изучить во время раунда на текущий момент
 cp14-skill-req-god-follower-percentage = Количество ваших последователей должно быть больше {$count}%
