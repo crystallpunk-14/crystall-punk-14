1eead99543d8142c9eec8bd1b6a32c157b355aa9
Red <96445749+TheShuEd@users.noreply.github.com>
2025-08-27 17:34:10 +0300

    You are surrounded by unknown suspicious individuals, my lord. (#1710)
    * base identity hide
    
    * ui
    
    * Better UI
    
    * store data in Mind
    
    * disable arrivals an leave announcement
    
    * tips updated
    
    * Enhance identity recognition and examination feedback
    
    Added logic to display remembered character names during examination and improved the process for remembering character names on map initialization. Also updated localization files to include new examination feedback strings. Minor refactoring and bug fixes in identity recognition systems.
    
    * Update CP14SharedIdentityRecognitionSystem.cs
    
    * fix

--- Content.Client/UserInterface/Systems/Chat/ChatUIController.cs
+++ Content.Client/UserInterface/Systems/Chat/ChatUIController.cs
@@ -1,6 +1,7 @@
 using System.Globalization;
 using System.Linq;
 using System.Numerics;
+using Content.Client._CP14.IdentityRecognition;
 using Content.Client.Administration.Managers;
 using Content.Client.Chat;
 using Content.Client.Chat.Managers;
@@ -820,6 +821,21 @@ public sealed partial class ChatUIController : UIController
 
     public void ProcessChatMessage(ChatMessage msg, bool speechBubble = true)
     {
+        //CP14 transform message on clientside
+        if (_player.LocalEntity is not null && msg.SenderEntity.IsValid())
+        {
+            var ev = new CP14ClientTransformNameEvent(msg.SenderEntity);
+            _ent.EventBus.RaiseLocalEvent(_player.LocalEntity.Value, ev);
+
+            if (ev.Handled)
+            {
+                var oldName = SharedChatSystem.GetStringInsideTag(msg, "Name");
+                var newName = ev.Name;
+                msg.WrappedMessage = msg.WrappedMessage.Replace($"[Name]{oldName}[/Name]", $"[Name]{newName}[/Name]");
+            }
+        }
+        //CP14 end
+
         // color the name unless it's something like "the old man"
         if ((msg.Channel == ChatChannel.Local || msg.Channel == ChatChannel.Whisper) && _chatNameColorsEnabled)
         {
---
+++ Resources/Locale/en-US/_CP14/identity/identity_recognition.ftl
@@ -0,0 +1,5 @@
+cp14-remember-name-verb = Remember name
+
+cp14-remember-name-name = Name
+
+cp14-remember-name-examine = You remember this character as [color=yellow]{$name}[/color]
