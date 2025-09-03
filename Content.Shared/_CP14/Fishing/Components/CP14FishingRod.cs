310126c660920d901e3affac4e9918eab0a82362
Deserty0 <MrDeserty@yandex.ru>
2025-09-04 02:32:59 +1000

    init

---
+++ Content.Shared/_CP14/Fishing/Components/CP14FishingRod.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._CP14.Fishing.Components;
+
+/// <summary>
+/// Allows to fish with this item
+/// </summary>
+[RegisterComponent]
+public sealed partial class CP14FishingRodComponent : Component
+{
+    
+}
