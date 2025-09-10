782ae7c5889f2de755db78faa04196249cb68f8d
Deserty0 <MrDeserty@yandex.ru>
2025-09-11 03:07:27 +1000

    Update CP14FishBaseBehavior.cs

--- Content.Shared/_CP14/Fishing/Behaviors/CP14FishBaseBehavior.cs
+++ Content.Shared/_CP14/Fishing/Behaviors/CP14FishBaseBehavior.cs
@@ -5,11 +5,17 @@ namespace Content.Shared._CP14.Fishing.Behaviors;
 [ImplicitDataDefinitionForInheritors]
 public abstract partial class CP14FishBaseBehavior
 {
-    public abstract void CalculatePosition(IRobustRandom random);
+    public abstract float CalculatePosition(IRobustRandom random, float cordA);
 
+    /// <summary>
+    /// Speed which fish uses when going from A to B +- 0.2*Speed
+    /// </summary>
     [DataField]
     public float Speed;
 
+    /// <summary>
+    /// How long fish will be in point B until selecting next point B +- 0.2*Difficulty
+    /// </summary>
     [DataField]
     public float Difficulty;
 }
