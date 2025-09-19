7c4dddd9895e05f881722d7a1495c1a9fb017bcc
Deatherd <russianihojin@gmail.com>
2025-09-19 19:57:24 +0300

    Species requirements : Start and Finish

---
+++ Content.Shared/_CP14/CCvar/CCvars.CP14Requirements.cs
@@ -0,0 +1,12 @@
+﻿using Robust.Shared.Configuration;
+
+namespace Content.Shared.CCVar;
+
+public sealed partial class CCVars
+{
+    /// <summary>
+    ///     If roundstart species should be restricted based on time.
+    /// </summary>
+    public static readonly CVarDef<bool>
+        RoundstartSpeciesTimers = CVarDef.Create("game.species_timers", true, CVar.SERVER | CVar.REPLICATED);
+}
---
+++ Content.Shared/_CP14/Species/SpeciesPrototype.Requirements.cs
@@ -0,0 +1,13 @@
+﻿using Content.Shared.Roles;
+
+// ReSharper disable once CheckNamespace
+namespace Content.Shared.Humanoid.Prototypes;
+
+public sealed partial class SpeciesPrototype
+{
+    /// <summary>
+    ///     Requirements for the species in the character editor.
+    /// </summary>
+    [DataField]
+    public HashSet<JobRequirement>? Requirements;
+}
