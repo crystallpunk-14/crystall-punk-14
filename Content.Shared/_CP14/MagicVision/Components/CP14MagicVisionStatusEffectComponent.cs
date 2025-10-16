da5d206697003032af6040e5fd934390367762ba
kin98 <benj.schleiss@bluewin.ch>
2025-10-16 15:13:37 +0200

    added Magic Vision Statuseffect component

---
+++ Content.Shared/_CP14/MagicVision/Components/CP14MagicVisionStatusEffectComponent.cs
@@ -0,0 +1,23 @@
+using Content.Shared.Eye;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._CP14.MagicVision;
+
+/// <summary>
+/// Allows to see magic vision trace entities
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class CP14MagicVisionStatusEffectComponent : Component
+{
+    /// <summary>
+    /// Enables or disables Magic Vision Overlay
+    /// </summary>
+    [DataField]
+    public bool Overlay = true;
+
+    /// <summary>
+    /// VisionMask to see Magic Vision layer
+    /// </summary>
+    [DataField]
+    public const VisibilityFlags VisionMask = VisibilityFlags.CP14MagicVision;
+}
