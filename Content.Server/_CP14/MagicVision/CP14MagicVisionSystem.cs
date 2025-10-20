d93b3424e0f60441f76b287ffb54ee4556194a75
kin98 <benj.schleiss@bluewin.ch>
2025-10-20 13:16:59 +0200

    marked MagicVisionComponent obsolete and changed protoid name

--- Content.Server/_CP14/MagicVision/CP14MagicVisionSystem.cs
+++ Content.Server/_CP14/MagicVision/CP14MagicVisionSystem.cs
@@ -12,14 +12,14 @@ public sealed class CP14MagicVisionSystem : CP14SharedMagicVisionSystem
     [Dependency] private readonly SharedEyeSystem _eye = default!;
     [Dependency] private readonly StatusEffectsSystem _status = default!;
 
-    private static EntProtoId _magicalVisionId = "CP14MetaMagicVisionSpellStatusEffect";
+    private static EntProtoId _magicalVisionSpellProtoId = "CP14MetaMagicVisionSpellStatusEffect";
 
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<MetaDataComponent, CP14MagicVisionToggleActionEvent>(OnMagicVisionToggle);
-        SubscribeLocalEvent<CP14MagicVisionStatusEffectComponent, GetVisMaskEvent>(OnGetVisMaskBody);
+        SubscribeLocalEvent<MetaDataComponent, CP14MagicVisionToggleActionEvent>(OnMagicVisionSpellToggle);
+        //SubscribeLocalEvent<CP14MagicVisionComponent, GetVisMaskEvent>(OnGetVisMaskBody); //Incase a non Status effect component
         SubscribeLocalEvent<EyeComponent, GetVisMaskEvent>(OnGetVisMask);
 
 
@@ -38,21 +38,24 @@ public sealed class CP14MagicVisionSystem : CP14SharedMagicVisionSystem
 
         args.VisibilityMask |= appliedMask;
     }
-    private void OnGetVisMaskBody(Entity<CP14MagicVisionStatusEffectComponent> ent, ref GetVisMaskEvent args)
+
+    //Incase there will be a component that applies Magic vision whitout a status Effect
+    [Obsolete($"Switched to Status Effect. Use {nameof(OnGetVisMask)} instead.", false)]
+    private void OnGetVisMaskBody(Entity<CP14MagicVisionComponent> ent, ref GetVisMaskEvent args)
     {
         var appliedMask = (int)CP14MagicVisionStatusEffectComponent.VisibilityMask;
 
         args.VisibilityMask |= appliedMask;
     }
-    private void OnMagicVisionToggle(Entity<MetaDataComponent> ent, ref CP14MagicVisionToggleActionEvent args)
+    private void OnMagicVisionSpellToggle(Entity<MetaDataComponent> ent, ref CP14MagicVisionToggleActionEvent args)
     {
-        if (!_status.CanAddStatusEffect(ent, _magicalVisionId))
+        if (!_status.CanAddStatusEffect(ent, _magicalVisionSpellProtoId))
             return;
 
-        if (_status.HasStatusEffect(ent, _magicalVisionId))
-            _status.TryRemoveStatusEffect(ent, _magicalVisionId);
+        if (_status.HasStatusEffect(ent, _magicalVisionSpellProtoId))
+            _status.TryRemoveStatusEffect(ent, _magicalVisionSpellProtoId);
         else
-            _status.TrySetStatusEffectDuration(ent, _magicalVisionId);
+            _status.TrySetStatusEffectDuration(ent, _magicalVisionSpellProtoId);
     }
 
     public override void Update(float frameTime)
--- Content.Shared/_CP14/MagicVision/Components/CP14MagicVisionComponent.cs
+++ Content.Shared/_CP14/MagicVision/Components/CP14MagicVisionComponent.cs
@@ -5,7 +5,7 @@ namespace Content.Shared._CP14.MagicVision;
 /// <summary>
 /// Allows to see magic vision trace entities
 /// </summary>
-[RegisterComponent, NetworkedComponent]
+[RegisterComponent, NetworkedComponent, Obsolete($"Changed to a Status Effect. Use {nameof(CP14MagicVisionStatusEffectComponent)} instead")]
 public sealed partial class CP14MagicVisionComponent : Component
 {
 }
