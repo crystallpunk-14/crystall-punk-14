27c696893928a30afe3f95f309bda2dbca154489
kin98 <benj.schleiss@bluewin.ch>
2025-10-24 19:52:39 +0200

    Added a Spell toggle status effect

---
+++ Content.Shared/_CP14/MagicSpell/Spells/CP14SpellToggleStatusEffect.cs
@@ -0,0 +1,28 @@
+using Content.Shared.StatusEffectNew;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._CP14.MagicSpell.Spells;
+
+public sealed partial class CP14SpellToggleStatusEffect : CP14SpellEffect
+{
+    [DataField(required: true)]
+    public EntProtoId StatusEffect = default;
+
+    [DataField]
+    public bool Toggle = false;
+
+    public override void Effect(EntityManager entManager, CP14SpellEffectBaseArgs args)
+    {
+        if (args.Target is null)
+            return;
+
+        var effectSys = entManager.System<StatusEffectsSystem>();
+
+        if (!Toggle)
+            effectSys.TrySetStatusEffectDuration(args.Target.Value, StatusEffect);
+        else
+            effectSys.TryRemoveStatusEffect(args.Target.Value, StatusEffect);
+
+        Toggle= !Toggle;
+    }
+}
