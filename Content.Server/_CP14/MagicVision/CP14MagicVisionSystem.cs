fee4c44b051a0a4a542dd09fd518e83840c8b923
kin98 <benj.schleiss@bluewin.ch>
2025-10-24 20:07:38 +0200

    fix leftover

--- Content.Server/_CP14/MagicVision/CP14MagicVisionSystem.cs
+++ Content.Server/_CP14/MagicVision/CP14MagicVisionSystem.cs
@@ -1,7 +1,6 @@
 using Content.Shared._CP14.MagicVision;
 using Robust.Shared.Timing;
 using Content.Shared.StatusEffectNew;
-using Robust.Shared.Prototypes;
 
 namespace Content.Server._CP14.MagicVision;
 
@@ -11,14 +10,10 @@ public sealed class CP14MagicVisionSystem : CP14SharedMagicVisionSystem
     [Dependency] private readonly SharedEyeSystem _eye = default!;
     [Dependency] private readonly StatusEffectsSystem _status = default!;
 
-    private static EntProtoId _magicalVisionSpellProtoId = "CP14MetaMagicVisionSpellStatusEffect";
-
     public override void Initialize()
     {
         base.Initialize();
 
-        SubscribeLocalEvent<MetaDataComponent, CP14MagicVisionToggleActionEvent>(OnMagicVisionSpellToggle);
-        //SubscribeLocalEvent<CP14MagicVisionComponent, GetVisMaskEvent>(OnGetVisMaskBody); //Incase a non Status effect component
         SubscribeLocalEvent<EyeComponent, GetVisMaskEvent>(OnGetVisMask);
 
         SubscribeLocalEvent<CP14MagicVisionStatusEffectComponent, StatusEffectAppliedEvent>(OnApplied);
